22ae5b3318c103824fa11da21aadbac4e3128cf5
Carl M. Kobel <kobel@pm.me>
2018-05-03 23:41:50 +0200

    overview plots and tweaks

--- plots2.r
+++ plots2.r
@@ -18,20 +18,42 @@ get_non_amp_reg = function(chromosome) {
 
 df = df %>%
     group_by(ind, chrom) %>%
-    mutate(normalized = count / df[df$gene == get_non_amp_reg(chrom) & df$ind == ind,]$count)
+    mutate(norm_count = count / df[df$gene == get_non_amp_reg(chrom) & df$ind == ind,]$count)
 View(df)
 
 setwd("png2")
-for(ichrom in c("X", "Y")) {
-    for (igene in unique(df[df$chrom==ichrom,]$gene)) { # for each unique gene-name
-        ggplot(df[df$chrom==ichrom & df$gene==igene,], aes(x=species, y=count)) +
-            geom_point() +
-            ggtitle(gene) +
-            labs(y=paste("CN / |", get_non_amp_reg(ichrom), "|", sep=""), x="species")
-        ggplot2::ggsave(paste(ichrom, "_gene_", igene, ".png", sep="")) # cowplot is masking ggsave
 
-        print(igene)
+# overview
+# del ind plotsene op i chromosom (så det er de samme gener sammen)
+
+
+# chrom species
+for(ichrom in unique(df$chrom)) {
+    for (ispecies in unique(df$species)) { # for each unique gene-name
+        ggplot(df[df$chrom==ichrom & df$species==ispecies,], aes(x=gene, y=norm_count, color=sex)) +
+            #geom_point(size=3) +
+            geom_jitter(width=0.1, height=0.00) +
+            #scale_y_continuous(limits = c(0,max(df$))) +
+            ggtitle(igene) +
+            labs(y=paste("number of copies"), x="species")
+        ggplot2::ggsave(paste(ichrom, "_species_", ispecies, ".png", sep=""), width=3, height=4)
+        print(ispecies)
         #print(get_non_amp_reg(ichrom))
     }
 }
 
+
+# chrom gene
+for(ichrom in unique(df$chrom)) {
+    for (igene in unique(df[df$chrom==ichrom,]$gene)) { # for each unique gene-name
+        ggplot(df[df$chrom==ichrom & df$gene==igene,], aes(x=species, y=norm_count, color=sex)) +
+            #geom_point(size=3) +
+            geom_jitter(width=0.1, height=0.00) +
+            #scale_y_continuous(limits = c(0,max(df$))) +
+            ggtitle(igene) +
+            labs(y=paste("number of copies"), x="species")
+        ggplot2::ggsave(paste(ichrom, "_gene_", igene, ".png", sep=""), width=3, height=4)
+        print(igene)
+        #print(get_non_amp_reg(ichrom))
+    }
+}
